54e863dfd49f2b969f736a20c2de59475837a7dc
Remoto <andyvasconcelos@ymail.com>
2023-08-06 05:32:43 -0300

    letsgo

--- pages/index.vue
+++ pages/index.vue
@@ -18,75 +18,6 @@
 
 <script>
 import InputForm from "@/components/InputForm.vue";
-import ResultItem from "@/components/ResultItem.vue";
-
-export default {
-  components: {
-    InputForm,
-    ResultItem
-  },
-  data() {
-    return {
-      showResults: false,
-      valorInvestimento: "",
-      tempoInvestimento: null,
-      tempos: [
-        {
-          text: "6 meses",
-          value: 6,
-          opcaoCallDivider: 0.016,
-          strikePrice: 0.03
-        },
-        // ... (rest of the tempos) ...
-      ],
-      opcaoCall: null,
-      opcaoPut: null,
-      strikePrice: null,
-      dataExercicio: null
-    };
-  },
-  methods: {
-    simularInvestimento() {
-      if (!this.valorInvestimento || !this.tempoInvestimento) {
-        alert(
-          "Por favor, insira o valor do investimento e escolha o tempo de investimento."
-        );
-        return;
-      }
-
-      const selectedTempo = this.tempos.find(
-        tempo => tempo.value === this.tempoInvestimento
-      );
-      if (!selectedTempo) {
-        alert("Tempo de investimento inválido.");
-        return;
-      }
-
-      const strikePrice = selectedTempo.strikePrice;
-      const currentDate = new Date();
-      const dataExercicio = new Date(currentDate);
-      dataExercicio.setMonth(dataExercicio.getMonth() + selectedTempo.value);
-
-      this.opcaoCall = (
-        this.valorInvestimento / selectedTempo.opcaoCallDivider
-      ).toFixed(2);
-      this.opcaoPut = (this.opcaoCall * strikePrice).toFixed(2);
-      this.strikePrice = strikePrice.toFixed(2);
-      this.dataExercicio = dataExercicio
-        .toLocaleDateString("pt-BR")
-        .slice(0, 10);
-
-      this.results = [
-        { title: "Opção de Venda", value: `R$ ${this.opcaoPut}` },
-        { title: "Preço de Exercício", value: `R$ ${this.strikePrice}` },
-        { title: "Data de Exercício", value: this.dataExercicio },
-        { title: "Opção de Compra", value: `${this.opcaoCall} EQ9` }
-      ];
-
-      this.showResults = true;
-    }
-  }
-};
 </script>
 
 <style>
@@ -103,11 +34,4 @@ export default {
   text-align: center;
 }
 
-/* Additional styles for grid layout */
-.grid {
-  display: grid;
-  grid-template-columns: repeat(auto-fit, minmax(300px, 1fr));
-  gap: 20px;
-  margin-top: 30px;
-}
 </style>
